c668f87d21af877941f7a8280adddfa60ef5f1cc
FilipePaixao <fpedrapaixao@gmail.com>
2026-05-11 13:44:47 -0300

    refactor: update user model to include ObjectId and updatedAt properties

--- src/infraestructure/db/mongo/models/user.model.ts
+++ src/infraestructure/db/mongo/models/user.model.ts
@@ -1,5 +1,10 @@
-import { model } from 'mongoose';
-import { IMUser } from '../interfaces/user.interface';
+import { Types, model } from 'mongoose';
+import { IUser } from '../../../../domain/user/entity/interfaces/user.interface';
 import { UserSchema } from '../schema/user.schema';
+
+export interface IMUser extends Omit<IUser, '_id'> {
+  _id: Types.ObjectId;
+  updatedAt: Date;
+}
 
 export const UserModel = model<IMUser>('User', UserSchema);
