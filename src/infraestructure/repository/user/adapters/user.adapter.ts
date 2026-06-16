b490ddc127561980bfc2672d84eb3cad3bb848af
FilipePaixao <fpedrapaixao@gmail.com>
2026-06-16 20:31:01 -0300

    feat: add user.adapter

--- src/infraestructure/repository/user/adapters/user.adapter.ts
+++ src/infraestructure/repository/user/adapters/user.adapter.ts
@@ -6,14 +6,30 @@ export function dbToInternal(user: IMUser): IUser {
     id: user.id,
     name: user.name,
     email: user.email,
+    passwordHash: user.passwordHash,
+    document: user.document,
+    salary: user.salary,
+    age: user.age,
     createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
   };
 }
 
-export function internalToDb(user: IUser): Omit<IMUser, '_id' | 'createdAt' | 'updatedAt'> {
+export function internalToDb(
+  user: IUser,
+): Omit<IMUser, '_id' | 'createdAt' | 'updatedAt'> {
   return {
     id: user.id,
     name: user.name,
     email: user.email,
+    passwordHash: user.passwordHash,
+    document: user.document,
+    salary: user.salary,
+    age: user.age,
   };
+}
+
+export function toUserPublic(user: IUser): Omit<IUser, 'passwordHash'> {
+  const { passwordHash: _passwordHash, ...publicUser } = user;
+  return publicUser;
 }
