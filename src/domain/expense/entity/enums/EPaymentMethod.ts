9eadeced175c8d7815853ecf7602eec461eacb1b
FilipePaixao <fpedrapaixao@gmail.com>
2026-06-16 20:30:49 -0300

    feat: add EPaymentMethod

---
+++ src/domain/expense/entity/enums/EPaymentMethod.ts
@@ -0,0 +1,9 @@
+export enum EPaymentMethod {
+  CREDIT_CARD = 'CREDIT_CARD',
+  DEBIT_CARD = 'DEBIT_CARD',
+  PIX = 'PIX',
+  CASH = 'CASH',
+  BANK_SLIP = 'BANK_SLIP',
+  BANK_TRANSFER = 'BANK_TRANSFER',
+  OTHER = 'OTHER',
+}
