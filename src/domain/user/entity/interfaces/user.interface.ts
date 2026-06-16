cf214e09ea3c007d5420512cc15af5ffdd8b081b
FilipePaixao <fpedrapaixao@gmail.com>
2026-06-16 20:30:42 -0300

    feat: add user.interface

--- src/domain/user/entity/interfaces/user.interface.ts
+++ src/domain/user/entity/interfaces/user.interface.ts
@@ -1,6 +1,33 @@
+import { IDocument } from './document.interface';
+import { ISalary } from './salary.interface';
+
 export interface IUser {
   id: string;
   name: string;
   email: string;
+  passwordHash?: string;
+  document?: IDocument;
+  salary?: ISalary;
+  age?: number;
   createdAt: Date;
+  updatedAt?: Date;
+}
+
+export interface IRegisterUserInput {
+  name: string;
+  email: string;
+  password: string;
+  document: IDocument;
+  age?: number;
+}
+
+export interface IUserPublicProfile {
+  id: string;
+  name: string;
+  email: string;
+  document?: IDocument;
+  salary?: ISalary;
+  age?: number;
+  createdAt: Date;
+  updatedAt?: Date;
 }
