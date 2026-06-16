21d1e08437cbef9a9cb54b4413de2203a2ced19a
FilipePaixao <fpedrapaixao@gmail.com>
2026-06-16 20:30:40 -0300

    feat: add postgres.client

---
+++ src/infraestructure/db/postgres/postgres.client.ts
@@ -0,0 +1,13 @@
+import { Pool, QueryResult } from 'pg';
+import { POSTGRES_URI } from '../../../configuration/env-constants/env.constants';
+
+export const postgresPool = new Pool({
+  connectionString: POSTGRES_URI,
+});
+
+export async function runPostgresQuery<T = unknown>(
+  query: string,
+  values: unknown[] = [],
+): Promise<QueryResult<T>> {
+  return postgresPool.query<T>(query, values);
+}
