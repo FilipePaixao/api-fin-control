4fa0eab459d949dbfae2a458717012b6aa19dc85
FilipePaixao <fpedrapaixao@gmail.com>
2026-06-19 16:35:14 -0300

    feat: add user service factory with profile dependencies

--- src/configuration/factory/user.service.factory.ts
+++ src/configuration/factory/user.service.factory.ts
@@ -1,6 +1,7 @@
 import { UserService } from '../../domain/user/service/user.service';
 import { UserRepositoryRead } from '../../infraestructure/repository/user/user.repository.read';
 import { UserRepositoryWrite } from '../../infraestructure/repository/user/user.repository.write';
+import { ConversationServiceFactory } from './conversation.service.factory';
 
 export class UserServiceFactory {
   static create() {
@@ -10,6 +11,7 @@ export class UserServiceFactory {
     return new UserService({
       userRepositoryRead: repoRead,
       userRepositoryWrite: repoWrite,
+      conversationService: ConversationServiceFactory.create(),
     });
   }
 }
