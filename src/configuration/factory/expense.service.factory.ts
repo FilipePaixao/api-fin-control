ed937163a84bb61420c1c3713881eb3366bad93b
FilipePaixao <fpedrapaixao@gmail.com>
2026-06-19 16:35:37 -0300

    feat: update expense service factory with search sync

--- src/configuration/factory/expense.service.factory.ts
+++ src/configuration/factory/expense.service.factory.ts
@@ -1,12 +1,18 @@
 import { ExpenseService } from '../../domain/expense/service/expense.service';
 import { ExpenseRepositoryRead } from '../../infraestructure/repository/expense/expense.repository.read';
 import { ExpenseRepositoryWrite } from '../../infraestructure/repository/expense/expense.repository.write';
+import { OpenSearchExpenseIndexRepository } from '../../infraestructure/repository/search/opensearch-expense-index.repository';
+import { ExpenseSearchServiceFactory } from './expense-search.service.factory';
+import { RagServiceFactory } from './rag.service.factory';
 
 export class ExpenseServiceFactory {
   static create(): ExpenseService {
     return new ExpenseService({
       expenseRepositoryRead: new ExpenseRepositoryRead(),
       expenseRepositoryWrite: new ExpenseRepositoryWrite(),
+      expenseSearchService: ExpenseSearchServiceFactory.create(),
+      expenseIndexRepository: new OpenSearchExpenseIndexRepository(),
+      ragService: RagServiceFactory.create(),
     });
   }
 }
