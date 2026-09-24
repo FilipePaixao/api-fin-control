ddff070a2badfe75fffd737c94e6fd4f471fc22b
FilipePaixao <fpedrapaixao@gmail.com>
2026-09-24 11:17:09 -0300

    feat(expense): include creditCardId in index document

--- src/domain/expense/utils/expense-index-document.utils.ts
+++ src/domain/expense/utils/expense-index-document.utils.ts
@@ -1,6 +1,7 @@
 import { IExpense } from '../entity/interfaces/expense.interface';
 import { getExpenseCategoryLabel } from '../utils/expense-labels.utils';
 import { IExpenseIndexDocument } from '../../expense-search/interfaces/expense-index.repository';
+import { buildExpenseSearchTerms } from '../../expense-search/utils/expense-search-enrichment.utils';
 
 export function toExpenseIndexDocument(expense: IExpense): IExpenseIndexDocument {
   return {
@@ -10,6 +11,7 @@ export function toExpenseIndexDocument(expense: IExpense): IExpenseIndexDocument
     description: expense.description,
     category: expense.category,
     categoryLabel: getExpenseCategoryLabel(expense.category),
+    searchTerms: buildExpenseSearchTerms(expense),
     status: expense.status,
     referenceMonth: expense.referenceMonth,
   };
